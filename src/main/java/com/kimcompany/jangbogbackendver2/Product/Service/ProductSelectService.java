7569b1a4b81758db584132029c62a617a191c9d5
김준영 <novb1492@naver.com>
2022-09-09 01:33:21 +0900

    이벤트 가격 적용

--- src/main/java/com/kimcompany/jangbogbackendver2/Product/Service/ProductSelectService.java
+++ src/main/java/com/kimcompany/jangbogbackendver2/Product/Service/ProductSelectService.java
@@ -3,15 +3,22 @@ package com.kimcompany.jangbogbackendver2.Product.Service;
 import com.kimcompany.jangbogbackendver2.Product.Dto.SearchCondition;
 import com.kimcompany.jangbogbackendver2.Product.Dto.SelectListDto;
 import com.kimcompany.jangbogbackendver2.Product.Repo.ProductRepo;
+import com.kimcompany.jangbogbackendver2.ProductEvent.Model.ProductEventEntity;
+import com.kimcompany.jangbogbackendver2.ProductEvent.Repo.ProductEventRepo;
 import com.kimcompany.jangbogbackendver2.Store.Dto.SelectNotyDto;
 import com.kimcompany.jangbogbackendver2.Store.Model.StoreEntity;
 import com.kimcompany.jangbogbackendver2.Store.Repo.StoreRepo;
 import com.kimcompany.jangbogbackendver2.Store.StoreSelectService;
 import com.kimcompany.jangbogbackendver2.Text.BasicText;
+import com.kimcompany.jangbogbackendver2.Util.UtilService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static com.kimcompany.jangbogbackendver2.Text.BasicText.deleteState;
 import static com.kimcompany.jangbogbackendver2.Text.BasicText.productListPageSize;
@@ -22,6 +29,7 @@ import static com.kimcompany.jangbogbackendver2.Text.BasicText.productListPageSi
 public class ProductSelectService {
     private final ProductRepo productRepo;
     private final StoreRepo storeRepo;
+    private final ProductEventRepo productEventRepo;
 
     public boolean exist(String productName,long storeId){
         return productRepo.exist(storeId,productName);
@@ -35,6 +43,14 @@ public class ProductSelectService {
         StoreEntity storeEntity = storeRepo.findInsteadOfId(searchCondition.getName(), searchCondition.getAddr(), deleteState).orElseThrow(() -> new IllegalArgumentException("등록되지 않은 매장입니다"));
         searchCondition.setStoreId(storeEntity.getId());
         searchCondition.setPageSize(1);
-        return productRepo.selectForList(searchCondition);
+        Page<SelectListDto> selectListDtos = productRepo.selectForList(searchCondition);
+        for(SelectListDto s:selectListDtos.getContent()){
+            ProductEventEntity productEventEntity = productEventRepo.findProductId(s.getId(), Timestamp.valueOf(LocalDateTime.now())).orElseGet(() -> null);
+            if(productEventEntity!=null){
+                s.setPrice(productEventEntity.getEventPrice());
+            }
+        }
+
+        return selectListDtos;
     }
 }
--- src/main/java/com/kimcompany/jangbogbackendver2/ProductEvent/Repo/ProductEventRepo.java
+++ src/main/java/com/kimcompany/jangbogbackendver2/ProductEvent/Repo/ProductEventRepo.java
@@ -2,6 +2,16 @@ package com.kimcompany.jangbogbackendver2.ProductEvent.Repo;
 
 import com.kimcompany.jangbogbackendver2.ProductEvent.Model.ProductEventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductEventRepo extends JpaRepository<ProductEventEntity,Long> {
+
+    @Query("select p from ProductEventEntity p where  p.productEntity.id=:id and p.startDate<=:now and p.endDate>=:now")
+    Optional<ProductEventEntity> findProductId(@Param("id")long productId, @Param("now") Timestamp now);
 }
